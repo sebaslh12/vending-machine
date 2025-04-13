5d0eb91cd5d2ea2e51b82dc8595c81a3fdbbb60d
Sebastian Lozano <sebaslh12@gmail.com>
2025-04-13 19:33:50 +0100

    Use Product and Change types

--- store/change.py
+++ store/change.py
@@ -1,18 +1,19 @@
 import json
 import os
 from decorators.file_exceptions import handle_file_operations
+from typings.change_types import Change
 
 __location__ = os.path.realpath(
 os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 @handle_file_operations
-def get_change_from_file(path_to_file='change.json'):
+def get_change_from_file(path_to_file='change.json') -> Change:
     with open(os.path.join(__location__, path_to_file), "r") as product_file:
         change_file = json.load(product_file)
-        return {int(key): val for key,val in change_file.items()}
+        return { int(key): val for key,val in change_file.items() }
 
 @handle_file_operations
-def save_change_to_file(change, path_to_file='change.json'):
+def save_change_to_file(change: Change, path_to_file='change.json'):
     print(f"Saving change state with: {change}")
     with open(os.path.join(__location__, path_to_file), "w", encoding='utf-8') as product_file:
         json.dump(change, product_file)
--- store/products.py
+++ store/products.py
@@ -1,17 +1,18 @@
 import json
 import os
 from decorators.file_exceptions import handle_file_operations
+from typings.product_types import Products
 
 __location__ = os.path.realpath(
 os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 @handle_file_operations
-def get_products_from_file(path_to_file='products.json'):
+def get_products_from_file(path_to_file='products.json') -> Products:
     with open(os.path.join(__location__, path_to_file), "r") as product_file:
         return json.load(product_file)
 
 @handle_file_operations
-def save_products_to_file(products, path_to_file='products.json'):
+def save_products_to_file(products: Products, path_to_file='products.json') -> None:
     print(f"Saving product state with: {products}")
     with open(os.path.join(__location__, path_to_file), "w", encoding='utf-8') as product_file:
         json.dump(products, product_file)
